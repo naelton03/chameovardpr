c251d19268c024058598e1e7f20c81bad4e9df50
naelton03 <55466855+naelton03@users.noreply.github.com>
2026-02-18 21:29:48 -0300

    Fix missing GoogleSignInStatusCodes import

--- app/src/main/java/com/example/replaycam/YouTubeLiveHandler.kt
+++ app/src/main/java/com/example/replaycam/YouTubeLiveHandler.kt
@@ -7,6 +7,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.Scope
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
